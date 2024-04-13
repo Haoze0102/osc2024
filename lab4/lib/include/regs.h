46a5fa5018c67632c9b4c84c67a22eb7ee2dccbd
Haoze <arthur1020099@gmail.com>
2024-04-13 12:41:47 +0800

    Init lab4

--- lab4/lib/include/regs.h
+++
@@ -1,9 +0,0 @@
-#ifndef __REGS_H__
-#define __REGS_H__
-
-#define CPACR_EL1_FPEN (0b11 << 20)
-#define get_reg(var_name, target)             \
-    asm volatile("mrs    %0, " #target "\n\t" \
-                 : "=r"(var_name))
-
-#endif
