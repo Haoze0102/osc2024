85b54cd9ca714995d64021adbcb7f9ebea93f320
Haoze <arthur1020099@gmail.com>
2024-05-17 14:29:29 +0800

    Let processes use kmalloc instead of s_allocator

--- lab6/kernel/src/main.c
+++ lab6/kernel/src/main.c
@@ -15,16 +15,18 @@ void main(char* arg){
     dtb_ptr = arg;
     traverse_device_tree(dtb_ptr, dtb_callback_initramfs); // get initramfs location from dtb
 
+    init_allocator();
+
     uart_init();
     irqtask_list_init();
     timer_list_init();
+
+    init_thread_sched();
 
     uart_interrupt_enable();
     el1_interrupt_enable();  // enable interrupt in EL1 -> EL1
     core_timer_enable();
 
-    init_allocator();
-    init_thread_sched();
 
     cli_print_banner();
     while(1){
